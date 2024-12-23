921847420b1a551da652958bcc18f7ae63eb17d1
Renata <renatacristovams@gmail.com>
2024-12-23 12:49:02 -0300

    Adds discount calculate option

--- app/src/main/java/br/com/lucramaisagenciadigital/registrapedidos/presentation/views/makesalescreen/components/ViewSales.kt
+++ app/src/main/java/br/com/lucramaisagenciadigital/registrapedidos/presentation/views/makesalescreen/components/ViewSales.kt
@@ -12,10 +12,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,8 +40,11 @@ import br.com.lucramaisagenciadigital.registrapedidos.ui.theme.DarkBlue
 fun ViewSales(
     modifier: Modifier = Modifier,
     saleItemsList: List<SaleItem>? = emptyList(),
-    onDeleteButtonClicked: (SaleItem) -> Unit
+    onDeleteButtonClicked: (SaleItem) -> Unit,
+    onCalculateDiscount: (Double) -> Unit
 ) {
+    val discountNumber = remember { mutableStateOf(String()) }
+
     Column(Modifier.background(Color.Yellow)) {
         Box(
             Modifier
@@ -73,6 +82,24 @@ fun ViewSales(
                             saleItem
                         ) {
                             onDeleteButtonClicked(it)
+                        }
+                    }
+                    item {
+                        TextField(
+                            value = discountNumber.value,
+                            onValueChange = { discountNumber.value = it },
+                            singleLine = true,
+                            placeholder = { Text(text = stringResource(id = R.string.discount)) },
+                            modifier = Modifier
+                                .padding(start = 16.dp, end = 16.dp, top = 4.dp)
+                                .fillMaxWidth()
+                        )
+                        Button(modifier = modifier
+                            .padding(start = 16.dp, end = 8.dp, bottom = 16.dp),
+                            onClick = {
+                                onCalculateDiscount(discountNumber.value.toDouble())
+                            }) {
+                            Text(text = stringResource(id = R.string.apply_discount))
                         }
                     }
                 }
@@ -129,5 +156,5 @@ fun ViewSalesPreview() {
             totalValue = 2100.00
         )
     )
-    ViewSales(saleItemsList = saleItems) {}
+    ViewSales(saleItemsList = saleItems, onDeleteButtonClicked= {_ ->}, onCalculateDiscount = {_ ->})
 }
